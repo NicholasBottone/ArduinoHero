e4bc9881edabe2c62717b5808c865e57919b29d1
Nicholas Bottone <nick@bottone.io>
2023-12-08 18:55:06 -0500

    Refactor song struct and include new song "All Star"

--- Parser/song.h
+++ Parser/song.h
@@ -9,13 +9,19 @@
     int year;
     char *genre;
     char *filename;
+    int resolution;
+    int sampling_rate;
 
-    float tempo; // milliseconds per beat
+    // Each float represents a bmp change during the song
+    float *bpm_values;
+    int *bpm_change_indexes;
+    int bpm_values_length;
 
     // Each byte represents a beat
     // 00000000 = no note
     // 00011111 = 5 fret note
     byte *beats;
+    int beats_length;
   };
 
 #endif
